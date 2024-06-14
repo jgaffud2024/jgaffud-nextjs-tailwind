74b1422b866164c3a2aff208b9482a668a81c532
jgaffud2024 <joseph.baybayinhub@gmail.com>
2024-06-14 23:47:47 +0800

    updated package.json , tailwindconfig.ts and hero.tx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,10 @@
 import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
-
-const config: Config = withMT({
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
+ 
+module.exports = withMT({
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 });
-
-export default config;
